711ad3fde437dc7f0f1ccdea5458dcc526c2a7dd
Chinmay Devadhar <cdevadhar@gmail.com>
2023-06-17 18:15:17 -0700

    get chat summaries frontend

--- frontend/Screens/ChannelScreen.js
+++ frontend/Screens/ChannelScreen.js
@@ -1,9 +1,20 @@
 import {View, Text} from "react-native";
+import {useState, useEffect} from "react";
+import axios from "axios";
+
 function ChannelScreen({route, navigation}) {
+	console.log(route.params.userName);
+	const [summary, setSummary] = useState('');
+	useEffect(() => {
+		axios.get(`https://3cd6-135-180-118-61.ngrok-free.app/get_chat_summary?username=${route.params.userName}`).then((response) => {
+			console.log(response.data);
+			setSummary(response.data);
+		})
+	}, []);
 	return (
 		<View>
 			<Text>
-				Channel Screen
+				{summary}
 			</Text>
 		</View>
 	)
