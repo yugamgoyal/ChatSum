d836ae4b4f913d2c64734edfe4ebe6c9c69808e0
Chinmay Devadhar <cdevadhar@gmail.com>
2023-06-17 22:53:54 -0700

    chennellist edits

--- frontend/Screens/ChannelList.js
+++ frontend/Screens/ChannelList.js
@@ -1,7 +1,9 @@
 import { View, Text, Pressable} from "react-native";
 import { useState, useEffect } from "react";
 import { ScrollView, TextInput } from "react-native-gesture-handler";
+import { Ionicons } from '@expo/vector-icons';
 import axios from 'axios';
+
 function filter(arr, str) {
 	return arr.filter((element) => {
 		let text = "";
@@ -11,18 +13,29 @@ function filter(arr, str) {
 		return false;
 	})
 }
+function randomColor() {
+	const colors = ["#FF5733", "#2E86C1", "#A93226", "#D4AC0D", "#229954", "#1C2833", "#6C3483"];
+	const index = Math.floor(Math.random()*colors.length);
+	return colors[index];
+}
 
 function ChannelListScreen({route, navigation}) {
 	const [chatArr, setChatArr] = useState([]);
 	const [searchTerm, setSearchTerm] = useState('');
 	const [displayArr, setDisplayArr] = useState([]);
-
+	const [colors, setColors] = useState({});
 	const [selectedChannel, setSelectedChannel] = useState(-1);
 	useEffect(() => {
+		
 		axios.get(`https://b59a-135-180-118-61.ngrok-free.app/get_users`).then((response) => {
 			console.log(response.data);
 			setDisplayArr(response.data);
 			setChatArr(response.data);
+			let colorsJSON = {};
+			for (let i=0; i<response.data.length; i++) {
+				colorsJSON[response.data[i].userName] = randomColor();
+			}
+			setColors(colorsJSON);
 		})
 	}, []);
 	
@@ -37,8 +50,8 @@ function ChannelListScreen({route, navigation}) {
 			}}
 			style={{
 				height: 50, 
-				borderWidth: 1, 
-				borderRadius: 5, 
+				// borderWidth: 1, 
+				borderRadius: 10, 
 				backgroundColor: "#ddd",
 				width: "100%",
 				alignSelf: "center",
@@ -57,11 +70,18 @@ function ChannelListScreen({route, navigation}) {
 							style={
 							{
 								padding: 10, 
-								height: 70, 
-								borderWidth: selectedChannel==key ? 3: 0.3, 
-								borderColor: selectedChannel==key ? "#44c": "black"
+								height: 90, 
+								borderWidth: selectedChannel==key ? 1: 0.25, 
+								borderColor: selectedChannel==key ? "#44c": "#ddd",
+								flexDirection: "row"
 							}}>
-							<Text style={{marginTop: "auto", marginBottom: "auto"}}>{chat.chatName!=null ? chat.chatName: chat.userName}</Text>
+							
+							{chat.chatName!=null ? <Ionicons name="people-circle" size={72} color={colors[chat.userName]} />: <Ionicons name="person-circle" size={72} color={colors[chat.userName]} />}
+							<View>
+								<Text style={{marginTop: 10, marginBottom: 5, marginLeft: 10, fontWeight: "bold", fontSize: 17}}>{chat.chatName!=null ? chat.chatName: chat.userName}</Text>
+								<Text style={{marginTop: 3, marginLeft: 10, color: "#888"}}>{Math.round(Math.random()*100)} new messages</Text>
+							</View>
+							
 						</Pressable>
 					})
 				}
